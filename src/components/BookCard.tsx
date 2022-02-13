41323eedb23f926d92e7adb3b8a7499e312c1c2f
clementgyimah <clementgyimah2@gmail.com>
2022-02-13 03:30:55 +0000

    feat: 🎸 book card component added

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,5 +1,28 @@
-const BookCard = () => {
-  return <div> Books </div>
+import * as React from 'react'
+import '../assets/css/BookCard.css'
+import { IBookCardProps } from '../types'
+import { BookDetailModal } from './BookDetailModal'
+
+const BookCard = (props: IBookCardProps) => {
+  const [showModal, setShowModal] = React.useState(false)
+
+  const openModal = () => {
+    setShowModal(true)
+  }
+  const closeModal = () => {
+    setShowModal(false)
+  }
+  return (
+    <div className="book-card-container">
+      <BookDetailModal show={showModal} close={closeModal} data={props.bookItem} />
+      <img
+        alt="bookCover"
+        src={props.bookItem.volumeInfo.imageLinks.thumbnail}
+        className="image-style"
+        onClick={openModal}
+      />
+    </div>
+  )
 }
 
 export { BookCard }
