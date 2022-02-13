7e2ea15ec60c38d80253760f7b423238c47195fc
clementgyimah <clementgyimah2@gmail.com>
2022-02-13 03:32:41 +0000

    feat: 🎸 tests moved to a new location

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { HomePage } from '../pages/home/HomePage'
+import { HomePage } from '../src/pages/home/HomePage'
 
 test('renders learn react link', () => {
   render(<HomePage />)
