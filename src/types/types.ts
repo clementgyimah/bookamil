ac0eb9d5fbbe691d65b893ec07048d3eed84391b
clementgyimah <clementgyimah2@gmail.com>
2022-02-13 03:23:59 +0000

    feat: 🎸 types added

---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+type TCloseModal = () => void
+type TMouseEvent = React.MouseEvent
+type TButtonAction = () => void
+
+export type { TCloseModal, TMouseEvent, TButtonAction }
